d7148a6eac6dd0ae8ec6478fe94c6573815c7ec1
Dev Ashrit Behera <devashritbehera@gmail.com>
2023-01-06 17:31:49 +0530

    tut-2

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Devswear - wear a smile" />
+        <title>devswear.com - wear a smile</title>
+        <meta name="description" content="devswear.com - wear a smile" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>  
+      </Head> 
+      <div className="mx-4">Dev is a good boy</div> 
     </>
   )
 }
